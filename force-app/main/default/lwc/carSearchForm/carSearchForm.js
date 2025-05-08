7fe32781b00f8d30614024b0dab28cd814ab1797
DamodarPatil <patildamodar1983@gmail.com>
2025-05-08 18:55:46 +0530

    Refactor CarSearchFormController and update carSearchForm.js to include getCarTypes import

--- force-app/main/default/lwc/carSearchForm/carSearchForm.js
+++ force-app/main/default/lwc/carSearchForm/carSearchForm.js
@@ -1,10 +1,11 @@
 import { LightningElement, track } from 'lwc';
+import getCarTypes from '@salesforce/apex/CarSearchFormController.getCarTypes'
 
 export default class CarSearchForm extends LightningElement {
     @track carTypes;
 
     handleCarTypeChange(event){
-        
+
     }
 
     createNewCarType(){
