36a67add1a1eca294c6e8ba03b8e83ece7d0ee43
DamodarPatil <patildamodar1983@gmail.com>
2025-05-09 11:01:00 +0530

    Refactor carSearchForm.js to include wire service for getCarTypes and enhance event handling

--- force-app/main/default/lwc/carSearchForm/carSearchForm.js
+++ force-app/main/default/lwc/carSearchForm/carSearchForm.js
@@ -1,14 +1,51 @@
-import { LightningElement, track } from 'lwc';
+import { LightningElement, track, wire } from 'lwc';
 import getCarTypes from '@salesforce/apex/CarSearchFormController.getCarTypes'
-
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import {NavigationMixin} from 'lightning/navigation';
 export default class CarSearchForm extends LightningElement {
     @track carTypes;
 
-    handleCarTypeChange(event){
+    @wire(getCarTypes)
+    wiredCarTypes({data, error}){
+        if(data){
+            this.carTypes = [{value:'', label:'All Types'}];
+            data.forEach(element => {
+                const carType = {};
+                carType.label = element.Name;
+                carType.value = element.Id;
+                this.carTypes.push(carType);
+             });
+        } else if(error){
+            this.showToast('ERROR', error.body.message, 'error');
+        }
+    }
 
+
+    handleCarTypeChange(event){
+        const carTypeId = event.detail.value;
+
+        const carTypeSelectionChangeEvent = new CustomEvent('cartypeselect',{
+            detail: carTypeId
+        });
+        this.dispatchEvent(carTypeSelectionChangeEvent);
     }
 
     createNewCarType(){
+        this[NavigationMixin.Navigate]({
+            type:'standard_objectPage',
+            attributes:{
+                objectApiName: 'Car_Type_c',
+                actionName: 'new'
+            }
+        });
+    }
 
+    showToast(title, message, variant){
+        const evt = new ShowToastEvent({
+            title: title,
+            message: message,
+            variant: variant,
+        });
+        this.dispatchEvent(evt);
     }
 }
