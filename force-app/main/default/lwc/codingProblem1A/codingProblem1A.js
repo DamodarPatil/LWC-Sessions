4cd12c4b2a2ab75de1e28517d8d43f3ac8bc5203
DamodarPatil <patildamodar1983@gmail.com>
2025-05-15 13:37:45 +0530

    Implement Active Contacts feature in CodingProblem1A component; add modal for contact list display and integrate data fetching from Apex controller

--- force-app/main/default/lwc/codingProblem1A/codingProblem1A.js
+++ force-app/main/default/lwc/codingProblem1A/codingProblem1A.js
@@ -1,8 +1,70 @@
-import { LightningElement, wire } from 'lwc';
-import getAccount from '@salesforce/apex/CodingProblem1Controller.getAccount';
+import { api, LightningElement } from "lwc";
+import getActiveContacts from "@salesforce/apex/CodingProblem1Controller.getActiveContacts";
+import { NavigationMixin } from "lightning/navigation";
 
-export default class CodingProblem1A extends LightningElement {
+export default class CodingProblem1A extends NavigationMixin(LightningElement) {
+  @api recordId;
+  contacts;
+  error;
 
-    @wire(getAccount)
-    accounts;
+  isModalOpen = false;
+
+  closeModalHandler() {
+    this.isModalOpen = false;
+  }
+
+  async loadContacts() {
+    try {
+      console.log(this.recordId);
+      this.contacts = await getActiveContacts({ accountId: this.recordId });
+      this.error = undefined;
+    } catch (error) {
+      this.error = error.message;
+      this.contacts = undefined;
+    }
+  }
+
+  handleButtonClick() {
+    this.isModalOpen = true;
+    this.loadContacts();
+  }
+
+  columns = [
+    {
+      label: "Name",
+      fieldName: "Name",
+      type: "button",
+      typeAttributes: {
+        label: { fieldName: "Name" },
+        name: "urlredirect",
+        variant: "base"
+      }
+    },
+    { label: "Department", fieldName: "Department" },
+    { label: "Email", fieldName: "Email" }
+  ];
+
+  handleRowAction(event) {
+    const actionName = event.detail.action.name;
+    const row = event.detail.row;
+    switch (actionName) {
+      case "urlredirect":
+        this.handleClickNavigation(row);
+        break;
+      default:
+        break;
+    }
+  }
+
+  handleClickNavigation(row) {
+    this.record = row;
+    this[NavigationMixin.Navigate]({
+      type: "standard__recordPage",
+      attributes: {
+        recordId: this.record.Id,
+        objectApiName: "Contact",
+        actionName: "view"
+      }
+    });
+  }
 }
